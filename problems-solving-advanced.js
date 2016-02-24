c0176d67cbc899d14d37d0f6027080933760ea7c
llwang8 <wangxbl56@gmail.com>
2016-02-24 10:38:44 -0500

    more than half done

---
+++ problems-solving-advanced.js
@@ -0,0 +1,204 @@
+//Validate US Telephone Numbers
+//Return true if the passed string is a valid US phone number
+//The user may fill out the form field any way they choose as long as it is a
+//valid US number. The following are examples of valid formats for US numbers
+//(refer to the tests below for other variants):
+function telephoneCheck(str) {
+  // Good luck!
+  return true;
+}
+
+
+
+telephoneCheck("555-555-5555");
+
+//Symmetric Difference
+//Create a function that takes two or more arrays and returns an array of the
+//symmetric difference (△ or ⊕) of the provided arrays.
+//Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the
+//mathematical term "symmetric difference" of two sets is the set of elements
+//which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every
+//additional symmetric difference you take (say on a set D = {2, 3}), you should get
+//the set with elements which are in either of the two the sets but not both
+//(C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
+function sym(args) {
+  return args;
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]);
+
+/*Exact Change
+Design a cash register drawer function that accepts purchase price as the
+first argument, payment as the second argument, and cash-in-drawer (cid) as the
+third argument.
+cid is a 2d array listing available currency.
+Return the string "Insufficient Funds" if cash-in-drawer is less than the change due.
+Return the string "Closed" if cash-in-drawer is equal to the change due.
+Otherwise, return change in coin and bills, sorted in highest to lowest order.
+*/
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  str = str.replace(/A-Z/, " /A-Z/").split(" ").join("-").toLowerCase();
+  str = str.toLowerCase().split(" ").join("-");
+  str = str.toLowerCase().split("_").join("-");
+  return str;
+}
+
+spinalCase("thisIsSpinalTap");
+
+
+
+
+/*Inventroy Update
+Compare and update inventory stored in a 2d array against a second 2d array of a
+fresh delivery. Update current inventory item quantity, and if an item cannot be
+found, add the new item and quantity into the inventory array in alphabetical order.
+*/
+function inventory(arr1, arr2) {
+    // All inventory must be accounted for or you're fired!
+    return arr1;
+}
+
+/* Example inventory lists
+Compare and update inventory stored in a 2d array against a second 2d array of a
+fresh delivery. Update current inventory item quantity, and if an item cannot be
+found, add the new item and quantity into the inventory array in alphabetical order.
+*/
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+inventory(curInv, newInv);
+
+
+/*No repeats please
+Return the number of total permutations of the provided string that don't have
+repeated consecutive letters. Assume that duplicate characters are each unique.
+For example, aab should return 2 because it has 6 total permutations (aab, aab,
+aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter
+(in this case a) repeating.
+*/
+function permAlone(str) {
+  return str;
+}
+
+permAlone('aab');
+
+
+/*Friendly Date Ranges
+Convert a date range consisting of two dates formatted as YYYY-MM-DD into a more
+readable format.
+
+The friendly display should use month names instead of numbers and ordinal dates
+instead of cardinal ("1st" instead of "1").
+
+Do not display information that is redundant or that can be inferred by the user:
+if the date range ends in less than a year from when it begins, do not display the
+ending year. If the range ends in the same month that it begins, do not display the
+ending year or month.
+
+Additionally, if the date range begins in the current year and ends within one year,
+the year should not be displayed at the beginning of the friendly range.
+*/
+function friendly(arr) {
+  return arr;
+}
+
+friendly(['2016-07-01', '2016-07-04']);
+
+
+
+
+/*Make a Person
+Fill in the object constructor with the methods specified in the tests.
+
+Those methods are getFirstName(), getLastName(), getFullName(), setFirstName(first),
+setLastName(last), and setFullName(firstAndLast).
+
+All functions that take an argument have an arity of 1, and the argument will be a
+string.
+
+These methods must be the only available means for interacting with the object.
+*/
+
+var Person = function(firstAndLast) {
+    return firstAndLast;
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
+
+
+
+/*Map the Debris
+Return a new array that transforms the element's average altitude into their
+orbital periods.
+
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+You can read about orbital periods on wikipedia.
+
+The values should be rounded to the nearest whole number. The body being orbited
+is Earth.
+
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is
+398600.4418
+*/
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+/*Pairwise
+Return the sum of all indices of elements of 'arr' that can be paired with one
+other element to form a sum that equals the value in the second argument 'arg'.
+If multiple sums are possible, return the smallest sum. Once an element has been
+used, it cannot be reused to pair with another.
+
+For example, pairwise([1, 4, 2, 3, 0, 5], 7) should return 11 because 4, 2, 3 and
+5 can be paired with each other to equal 7.
+
+pairwise([1, 3, 2, 4], 4) would only equal 1, because only the first two elements
+can be paired to equal 4, and the first element has an index of 0!
+*/
+function pairwise(arr, arg) {
+  return arg;
+}
+
+pairwise([1,4,2,3,0,5], 7);
+
+
+
+
+
+
+//A thinking Ape
+/*
+PUZZLE
+
+Imagine you have a circle of people and you go around the circle removing every second person until one person is left.
+
+If you have 3 people in the circle, then the 3rd person will be the last one remaining.
+If you have 4 people then the 1st person will be the last one remaining.
+If you have 11 people then the 7th person will be the one remaining.
+
+If you have N people in the circle, who will be the last one remaining?
+*/
+
+
+
+
--- problems-sovling-intermediate.js
+++ problems-sovling-intermediate.js
@@ -6,10 +6,64 @@
 //Diff Two Arrays
 //Compare two arrays and return a new array with any items only found in one of
 //the two given arrays, but not both. (return the symmetric fiff of the two arrays)
+function diff(arr1, arr2) {
+  var newArr = [];
+  newArr = arr1.concat(arr2).sort();
+  console.log(newArr);
+  //var result = newArr.filter(notSame(val, index, newArr));
+  var result = Array.prototype.filter.call(newArr, notSame);
+  // Same, same; but different.
+  return result;
+}
 
+function notSame(val, index, arr) {
+  if (arr[-1]  == val) {
+    return true;
+  }
+  if (arr[index + 1] == val) {
+    if (arr[index + 1] == arr[-1]) {
+      arr.slice(index+1);
+      return false;
+    } else {
+      arr.slice(index+1, index+2);
+      return false;
+    }
+  }
+}
+
+diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
 //Roman Numeral Converter
 //convert the given number into a roman numeral
+function convert(num) {
+  var single = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+  var tens = ["X", "XX", "XXX", "XL", "LX", "LXX", "LXXX", "CX"];
+  var hundreds = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  var thousands = Math.floor(num/1000);
+  var roman = [];
+  roman.push(hundreds);
+  roman.push(tens);
+  roman.push(single);
+  var result = "";
+  if (thousands !== 0) {
+    for (var i=0; i<thousands; i++) {
+      result.push("M");
+    }
+  }
+  if (num > 1000) {
+    num = num - thousands * 1000;
+  }
+  var arr = num.toString().split(); //change (num - thousands) to array
+  var n = arr.length;  // how many digits,
+  for (var j=0; j<n; j++) {
+    var index = arr[j] -1;
+    result.push(roman[-n][index]);
+    n += 1;
+  }
+
+ return result.join();
+}
+
 
 
 //Where art thou
@@ -88,7 +142,22 @@ where([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b
 //Sorted Union
 //Write a function that takes two or more arrays and returns a new array of unique
 //values in the order of the original provided arrays
+function unite(arr1, arr2, arr3) {
+  var arr = [];
+  for(var i=0; i<arguments.length; i++) {
+    arr.push(arguments[i]);
+  }
+  arr = arr.reduce(function(a, b) {
+    return a.concat(b);
+  }, []);
+  var result = arr.filter(unique);
+  return result;
+}
+function unique(elem, index, self) {
+    return self.indexOf(elem) == index;
+}
 
+unite([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 
 //Convert HTML Entities
 //Convert the charater &, <, >, ", ' in a string to their corresponding HTML entities
@@ -100,7 +169,7 @@ where([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
-  str = str.replace(/([a-z])([A-Z])/, "$1-$2").toLowerCase;
+  str = str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
 
   str = str.toLowerCase().split(" ").join("-");
   str = str.toLowerCase().split("_").join("-");
@@ -109,33 +178,31 @@ function spinalCase(str) {
 
 //Sum all odd fibonacci numbers up to and including the passed number if it is
 //Fibonocci number
-function sumFibs(num) {
-    var arr = [1, 1];
+ function sumFibs(num) {
     var sum = 0;
-    for(var i=2; i++) {
-        var n = arr.[i-1] + arr.[i-2];
-        if (n<= num) {
-            arr.push();
-        } else {
-            break;
-        }
+    var prefib = 0;
+    var curfib = 1;
+    while (curfib <= num) {
+      if (curfib % 2 !== 0) {
+            sum += curfib;
+       }
+       temp = prefib + curfib;
+      prefib = curfib;
+      curfib = temp;
     }
-    sum = arr.reduce(function(a,b){
-        if (b % 2 === 0) {
-            return a + b;
-        }
-    });
-    return sum;
+
+   return sum;
 }
 
 sumFibs(4)
 
 //Sum all primes numbers up to and including the  providing number
+//(divide and conquer algorithm)
 
 function sumPrimes(num) {
     var arr = [];
     var sum = 0;
-    main:for (var i=1; i<= num; i++) {
+    main:for (var i=2; i<= num; i++) {
         for (var j=2; j<i; j++){
             if (i%j === 0) {
                 continue main;
@@ -146,7 +213,7 @@ function sumPrimes(num) {
 
     sum = arr.reduce(function(a,b){
         return a + b;
-    })
+    });
     return sum;
 }
 
@@ -154,31 +221,115 @@ function sumPrimes(num) {
 //both, as well as by all sequential numbers in the range between these parameters
 //The range will be an array of two numbers that will not necessarily be in numerical
 //order
-function smalllestCommons(arr) {
-    arr = arr.sort(function(a,b) {return a - b});
-    var set = []
-    var multiples = [];
-    for (var i = arr[0]; i <= arr[1]; i++) {
-        set.push(i);
+function smallestCommons(arr) {
+    var range = createRange(arr);
+    var j = range[range.length-1];
+    while(true) {
+       var passed = range.every(function(multiple){
+          return j % multiple === 0;
+       });
+      if (passed) {
+        return j;
+      } else {
+        j++;
+      }
     }
-    m:for (var j = arr[0] ** 2; j < arr[1] ** 2; j++) {
-        for (var i = 0; i < set.length; i++) {
-            if (j % set[i] != 0) {
-                continue m;
-            }
+
+    function createRange() {
+        var range = [];
+        var max = arr.reduce(function(a, b){
+            return Math.max(a, b);
+        });
+        var min = arr.reduce(function(a, b){
+            return Math.min(a, b);
+        });
+        for (var i = min; i <=max; i++) {
+            range.push(i);
         }
-       multiples.push(j);
+        return range;
     }
-    return multiples.sort(function(a,b){return a -b});
+
+}
+// that Eucliden gcd algorithm
+function smallestCommons(array) {
+    var range = createRange(array);
+
+    return range.reduce(function(a, b) {
+        return lcm(a, b);
+    });
+
+    function lcm(a, b) {
+        return (Math.abs(a * b) / gcd(a, b));
+    }
+
+    function gcd(a, b) {
+        var temp;
+        while (b != 0) {
+            temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    function createRange(array) {
+        var range = [];
+        var highest = array.reduce(function(a, b) {
+            return Math.max(a, b);
+        });
+        var lowest = array.reduce(function(a, b) {
+            return Math.min(a, b);
+        });
+        for (var i = lowest; i <= highest; i++) {
+            range.push(i);
+        }
+        return range;
+    }
+}
+//another version
+function leastCommonMultiple(arr) {
+    /*
+      function range(min, max) {
+        var arr = [];
+       for (var i = min; i <= max; i++) {
+        arr.push(i);
+      }
+       return arr;
+    }
+    */
+    var min, range;
+     range = arr;
+    if(arr[0] > arr[1]){
+       min = arr[1];
+    }
+    else{
+       min = arr[0]
+    }
+
+    function gcd(a, b) {
+        return !b ? a : gcd(b, a % b);
+    }
+
+    function lcm(a, b) {
+        return (a * b) / gcd(a, b);
+    }
+
+   var multiple = min;
+    range.forEach(function(n) {
+       multiple = lcm(multiple, n);
+    });
+
+   return multiple;
 }
 
 
 //Finders Keepers
-//create a function that looks through an array (first args) and returns the first element
-//in the array that passes a truth test (2nd args)
-function find(arr, func) {
-    var num = 0;
-    return num;
+//create a function that looks through an array (first args) and returns the first
+//element in the array that passes a truth test (2nd args)
+function find(arr, func) {     //??
+    var num = [];
+    num = arr.filter(func);
+    return num.shift();
 }
 
 find([1, 2,3,4], function(num){return num%2 === 0;});
@@ -188,14 +339,33 @@ find([1, 2,3,4], function(num){return num%2 === 0;});
 //(2nd args) return true
 //return the rest of the array, otherwise return an empty array
 function drop(arr, func) {
+    for (var i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            return arr.slice(i);
+        }
+    }
+    return [];
 
-    return arr;
 }
+
 drop([1,2,3], function(n){return n < 3;});
 
 
 //Steamroller
 //Flatten a nested array. You must account for varying levels of nesting
+function steamroller (arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      var temp = steamroller(arr[i]);
+      temp.forEach(function(value){ newArr.push(value); })
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
 
 
 //Binary Agents
@@ -208,9 +378,15 @@ drop([1,2,3], function(n){return n < 3;});
 //Check if the predicate (2nd args) is truthy on all elements of a collection
 //(first args)
 
+
+
+
 //Arguments Optional
 //Create a function that sums two arguments together.  If only one argument is
 //provide, then return a function that expects one argument and return the sum.
+
+
+
 
 
 
