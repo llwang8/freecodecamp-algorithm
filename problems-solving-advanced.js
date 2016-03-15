7a60233077bb63d045816b6f353bec1b51974156
llwang8 <wangxbl56@gmail.com>
2016-03-14 20:30:31 -0400

    almost working permutationAlone

--- problems-solving-advanced.js
+++ problems-solving-advanced.js
@@ -69,6 +69,8 @@ Otherwise, return change in coin and bills, sorted in highest to lowest order.
 
 
 
+
+
 /*Inventroy Update
 Compare and update inventory stored in a 2d array against a second 2d array of a
 fresh delivery. Update current inventory item quantity, and if an item cannot be
@@ -116,7 +118,7 @@ var newInv = [
 
 inventory(curInv, newInv);
 
-
+//====================================================
 /*No repeats please
 Return the number of total permutations of the provided string that don't have
 repeated consecutive letters. Assume that duplicate characters are each unique.
@@ -147,8 +149,128 @@ function permAlone(str) {
 }
 
 permAlone('aab');
+//Heap's Algorithm
+//Heap's algorithm generates all possible permutations of N objects. It was
+//first proposed by B. R. Heap in 1963
+function permAlone(str) {
+  var i, j, n, arr, stack, result, count, final;
+  if (typeof str != "string"){
+    return "Input should be an array or a string.";
+  }
+  arr = str.split("");
+  stack = arr;
+  result = [];
+  n = arr.length;
+  count = 0;
+  for (j=0; j<n; j++){
+    for (i=1; i<n; i++){
+      if (count == permCount(n-1)-1){
+        break;
+      }else if (i == n-1) {
+        i = 1;
+      }
+      swap(0, i);
+      count ++;
+    }
+    if (n % 2 === 0){
+      if(j == n-1){
+        result.push(str);
+        break;
+      }
+       swap(j, n-1);
+    }else {
+       swap(0, n-1);
+    }
+    count = 0;
+  }
+  return result;
+  final = result.filter(nonRepeatConsecutive);
+  //return final;
+  return final.length;
 
+  function swap(p, q){
+    var temp = stack[p];
+    stack[p] = stack[q];
+    stack[q] = temp;
+    result.push(stack.join(""));
+  }
 
+  function permCount(num){
+    if (num == 1){
+      return 1;
+    }
+    return num * permCount(num-1);
+  }
+
+  function nonRepeatConsecutive(string){
+    /*
+    for(var k=0; k<string.length; k++){
+      if(string.charAt[k] == string.charAt[k+1]){
+        return false;
+      }
+    }
+    return true;
+    */
+    var repeats = /(.)\1/;
+    return !repeats.test(string);
+  }
+}
+
+permAlone('abc');
+
+var result = ["aabb", "baab", "abab", "abab", "baab", "baab", "abab", "abab", "baab", "aabb",
+"aabb", "abba", "baba", "baba", "abba", "bbaa", "bbaa", "bbaa", "bbaa", "abba", "baba", "baba", "abba", "aabb"];
+var final = result.filter(nonRepeatConsecutive);
+function nonRepeatConsecutive(string){
+  var repeats = /(.)\1/;
+  return !repeats.test(string);
+}
+console.log(final);
+
+//by Sabba Kilam
+/*
+Some of the names are long, but descriptive.
+
+Features:
+
+No longer get the "too much recursion" message.
+Only one argument is passed to the function.
+Takes a string or a number argument.
+Variables are not global but enclosed in the outer function.
+The inner function does the recursion using closure variables
+Doesn't use splice, but uses slice only once.
+*/
+//====================================================
+function getPermutations(str){
+    //Enclosed data to be used by the internal recursive function permutate():
+    var permutations = [],  //generated permutations stored here
+        nextWord = [],      //next word builds up in here
+        chars = []          //collection for each recursion level
+    ;
+    //---------------------
+    //split words or numbers into an array of characters
+    if (typeof str === 'string') chars = str.split('');
+    else if (typeof str === 'number') {
+      str = str + ""; //convert number to string
+      chars = str.split('');//convert string into char array
+    }
+    //============TWO Declaratives========
+    permutate(chars);
+    return permutations;
+    //===========UNDER THE HOOD===========
+    function permutate(chars){ //recursive: generates the permutations
+        if(chars.length === 0)permutations.push(nextWord.join(''));
+        for (var i=0; i < chars.length; i++){
+            chars.push(chars.shift());  //rotate the characters
+            nextWord.push(chars[0]);    //use the first char in the array
+            permutate(chars.slice(1));  //Recurse: array-less-one-char
+            nextWord.pop();             //clear for nextWord (multiple pops)
+        }
+    }
+    //--------------------------------
+}//==============END of getPermutations(str)=============
+
+//======================================================
 /*Friendly Date Ranges
 Convert a date range consisting of two dates formatted as YYYY-MM-DD into a more
 readable format.
@@ -165,7 +287,27 @@ Additionally, if the date range begins in the current year and ends within one y
 the year should not be displayed at the beginning of the friendly range.
 */
 function friendly(arr) {
-  return arr;
+  var temp;
+  var result = [];
+  arr[0] = arr[0].split("-");
+  arr[1] = arr[1].split("-");
+  if(arr[0][0] == arr[1][0]){
+
+  }else {
+    result.push(formatDate(arr[0]));
+    result.push(formatDate(arr[1]));
+  }
+  return result;
+  function formatDate(date){
+
+  }
+
+  function formatMonth(str){
+
+  }
+  function formatDay(str){
+
+  }
 }
 
 friendly(['2016-07-01', '2016-07-04']);
