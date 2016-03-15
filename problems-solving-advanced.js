830877ec8a3e21b1f710385e74fa07bd3fc638f4
llwang8 <wangxbl56@gmail.com>
2016-03-14 22:51:25 -0400

    friendly Date range

--- problems-solving-advanced.js
+++ problems-solving-advanced.js
@@ -286,33 +286,106 @@ ending year or month.
 Additionally, if the date range begins in the current year and ends within one year,
 the year should not be displayed at the beginning of the friendly range.
 */
-function friendly(arr) {
-  var temp;
+function friendly(arrDate) {
+  var i;
+  var arr = arrDate;
+  transform();
+  //return arr;
+  var n =  arr.length;
   var result = [];
-  arr[0] = arr[0].split("-");
-  arr[1] = arr[1].split("-");
-  if(arr[0][0] == arr[1][0]){
+  var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novement", "December"];
+  var today = new Date();
+  var currentYear = today.getFullYear();
 
-  }else {
-    result.push(formatDate(arr[0]));
-    result.push(formatDate(arr[1]));
-  }
+  if ((arr[0][2] == currentYear) && (arr[1][2] == currentYear)){  //same current year
+      arr[0][2] = "";
+      arr[1][2] = "";
+   }else if(arr[0][2] == arr[1][2]){   //same year
+      arr[0][2] = "";
+   }
+
+   if(arr[0][0] == arr[1][0]){  //same month
+      arr[1][1] = "";
+   }
+  //return arr;
+
+  formatDate();
   return result;
-  function formatDate(date){
+
+  function transform(){
+    for (i=0; i<n; i++){
+      arr[i] = arr[i].split("-");
+      arr[i][1] = parseInt(arr[i][1]);
+      arr[i][2] = parseInt(arr[i][2]);
+      arr[i].push(arr[i].shift());
+    }
+    return arr;
+  }
+  function formatDate(){
+    var temp;
+    for(var i=0; i<n; i++){
+      temp = "";
+      if (arr[i][0]){
+        temp += monthName[arr[i][0]] + " " + formatDate(arr[i][1]);
+      }
+      if (arr[i][2]) {
+        temp += ", " + arr[i][2];
+      }
+      result.push(temp);
+    }
 
   }
 
-  function formatMonth(str){
-
-  }
-  function formatDay(str){
-
+  function formatDay(num){
+    var date = "";
+    switch (num) {
+      case 1:
+        date = "1st";
+        break;
+      case 2:
+        date = "2nd";
+        break;
+      case 3:
+        date = "3rd";
+        break;
+      case 21:
+        date = "21st";
+        break;
+      case 22:
+        date = "22nd";
+        break;
+      case 23:
+        date = "23rd";
+        break;
+      default:
+        date = num + "th";
+    }
+    return date;
   }
 }
 
 friendly(['2016-07-01', '2016-07-04']);
 
+friendly(['2016-07-01', '2016-07-04']);
 
+
+//test
+var arrTest = ['2016-07-01', '2016-07-04'];
+function transform(arr){
+  var n =  arr.length;
+    for (i=0; i<n; i++){
+      arr[i] = arr[i].split("-");
+      console.log("after split: " + arr[i]);
+      arr[i][1] = parseInt(arr[i][1]);
+      arr[i][2] = parseInt(arr[i][2]);
+
+      arr[i].push(arr[i].shift());
+      console.log("after shift: " + arr[i]);
+    }
+  return arr;
+}
+arrTest = transform(arrTest);
+console.log(arrTest[0]);
 
 
 /*Make a Person
